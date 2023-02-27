328bf88031eaa674b3b36e5278c682c13609a502
Eliot Bianchi <bianchieliot68000@gmail.com>
2023-02-27 12:03:40 +0100

    Refactoring

--- server/server_auth.js
+++ server/server_auth.js
@@ -1,16 +1,12 @@
 require('dotenv').config()
+const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
-const pool = require('./db')
+app.use(cors())
+const pool = require('../server_auth/db')
 
 
 app.post('/log-in', async (req,res) => {
@@ -35,7 +31,7 @@ app.post('/log-in', async (req,res) => {
 })
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT_AUTH
 app.listen({ port: PORT }, () => {
     console.log(`Listen on port ${PORT}`)
 })
